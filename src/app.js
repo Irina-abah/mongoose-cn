c8220c3bd1a11a26606a54219fd857d7c8f42e8a
Irina Abah <irin.bon@gmail.com>
2022-02-04 10:57:07 +0000

    create and list work correctly

--- src/app.js
+++ src/app.js
@@ -13,10 +13,14 @@ const app = async () => {
       actor: argv.actor
     })
   } else if (argv.list) {
-    list()
+    await list()
     console.log("list")
   } else if (argv.delete) {
-    deleteMovie()
+    await deleteMovie()
+    // await deleteMovie({ 
+    //   name: argv.title,
+    //   actor: argv.actor })
+    
   }
   else {
     console.log("wrong command")
--- src/film/filmMethods.js
+++ src/film/filmMethods.js
@@ -13,20 +13,26 @@ exports.addMovie = async (newFilm) => {
 
 exports.list = async () => {
   try {
-    const allMovies = await Film.find({})
+    const allMovies = await Film.find({});
     console.log(allMovies)
   } catch (error) {
     console.log(error)
   }
 }
 
-exports.deleteMovie = async (newFilm) => {
+exports.deleteMovie = async () => {
   try {
     const allMovies = await Film.find({})
     console.log(allMovies)
-    await allMovies.findById({id: newFilm._id}).deleteOne()
+    await allMovies.findById({id: movie._id}).deleteOne()
+    // let movie = new Film(newFilm);
+    // await movie.deleteOne();
     console.log("Movie was deleted")
   } catch (error) {
     console.log(error)
   }
+}
+
+exports.updateMovie = async (movie) => {
+
 }
